cc4805528b31dfaea94cf6c15cbe8ec6886ebece
Ayussh <ayusshkhurana7@gmail.com>
2023-03-14 21:47:27 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,23 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "alert") {
-      chrome.notifications.create({
-        type: "basic",
-        title: "Adult Website Alert",
-        message: "Police aa gayi Pollice ",
-        iconUrl: "icon.png"
+(function() {
+  // array of adult keywords
+  const adultKeywords = ["adult", "porn", "sex", "xxx", "nsfw"];
+
+  // listen for page load
+  window.addEventListener("load", function() {
+    // check if the current URL contains an adult keyword
+    if (adultKeywords.some(keyword => window.location.href.includes(keyword))) {
+      // create a button to unblock adult websites
+      const button = document.createElement("button");
+      button.innerText = "Unblock Adult Websites";
+      button.style.position = "fixed";
+      button.style.bottom = "20px";
+      button.style.right = "20px";
+      button.style.zIndex = "9999";
+      button.addEventListener("click", function() {
+        // send a message to the background script to unblock adult websites
+        chrome.runtime.sendMessage({action: "unblock"});
       });
-      var audio = new Audio("alert.mp3");
-      audio.play();
+      document.body.appendChild(button);
     }
   });
-  
+})();
